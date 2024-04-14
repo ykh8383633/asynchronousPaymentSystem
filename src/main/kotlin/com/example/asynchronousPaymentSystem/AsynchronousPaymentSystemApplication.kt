5e72283eda3b8c530a223034b61fe1943013de90
ykh8383633 <ykh8383633@naver.com>
2024-04-14 23:24:39 +0900

    first commit

---
+++ src/main/kotlin/com/example/asynchronousPaymentSystem/AsynchronousPaymentSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.example.asynchronousPaymentSystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AsynchronousPaymentSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<AsynchronousPaymentSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/asynchronousPaymentSystem/AsynchronousPaymentSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.asynchronousPaymentSystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AsynchronousPaymentSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
